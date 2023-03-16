88dfe104bda92fbb766dc4a520f174aee1daef12
mehdi salim <mehdisalimofficial2000@gmail.com>
2023-03-16 04:41:55 -0700

    add description to main function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
