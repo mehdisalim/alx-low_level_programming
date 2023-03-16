2e822bdd1e8f75068036f765bffc3870be026704
mehdi salim <mehdisalimofficial2000@gmail.com>
2023-03-16 04:40:52 -0700

    add description to main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*
- * write "\"Programming is like building a multilingual puzzle" in the standard output 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
