08a9f4e2195a3d6fe5a1630bba562a8ef6efba63
mehdi salim <mehdisalimofficial2000@gmail.com>
2023-03-16 04:25:19 -0700

    first push

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+	printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+}
