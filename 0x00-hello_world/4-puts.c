84e0e623c46df1364a5e3b1f358f4da99f44424d
mehdi salim <mehdisalimofficial2000@gmail.com>
2023-03-16 04:39:32 -0700

    add description to main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ * write "\"Programming is like building a multilingual puzzle" in the standard output 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
